51ba8ac00bfbcf80259754d98fb28e0d1d62104d
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-26 13:57:46 -0600

    ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+    int uno, dos, tres;
+    printf ("Ingrese 3 números separados por espacios:\n"); 
+    scanf ("%d %d %d", &uno, &dos, &tres);
+    if (uno > dos)
+    {
+        if (dos > tres) 
+        {
+            printf("%d es mayor a %d que es mayor a %d\n", uno, dos, tres);     
+        }
+        else
+        {
+            if (uno > tres)
+            {
+                printf("%d es mayor a %d que es mayor a %d\n", uno, tres, dos);
+            }
+            else
+            {
+                printf("%d es mayor a %d que es mayor a %d\n", tres, uno, dos);
+            }
+        }
+    }
+    else
+    {
+        if (dos > tres) 
+        {
+            if (tres > uno) 
+            {
+                printf("%d es mayor a %d que es mayor a %d\n", dos, tres, uno); 
+            }
+            else
+            {
+                printf("%d es mayor a %d que es mayor a %d\n", dos, uno, tres);
+            } 
+        }
+        else 
+        {
+            printf("%d es mayor a %d que es mayor a %d\n", tres, dos, uno);
+        }
+    }
+    return 0; 
+}
+
