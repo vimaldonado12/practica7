210560ae88d4a6b8231ab198930399af37a8f1d8
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-26 14:01:58 -0600

    ej.c

---
+++ ej.c
@@ -0,0 +1,14 @@
+switch (opcion_a_evaluar) 
+{
+  case valor1:
+    /* Código a ejecutar*/
+  break;
+  case valor2:
+    /* Código a ejecutar*/
+  break;
+  case valorN:
+    /* Código a ejecutar*/
+  break;
+  default:
+    /* Código a ejecutar*/
+}
