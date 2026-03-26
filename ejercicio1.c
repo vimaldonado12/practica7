1ef9838315a3ae5cb6de8cc61ead5184e9dc1f45
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-26 13:30:57 -0600

    ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () 
+{
+  int a, b; 
+  a = 3;
+  b = 2;
+  if (a > b) 
+  {
+    printf("\ta (%d) es mayor a b (%d).\n",a,b); 
+  }
+  printf("\tEl programa sigue su flujo.\n");
+  return 0;  
+}
